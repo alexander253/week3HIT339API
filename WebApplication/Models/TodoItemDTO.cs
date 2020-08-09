f9f748e3adf5aa9d4cd50dbfe75957f3a3de0608
unknown <alex_s253@hotmail.com>
2020-08-09 16:32:24 +0930

    created DTO

--- WebApplication/Models/TodoItemDTO.cs
+++ WebApplication/Models/TodoItemDTO.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication.Models
 {
-    public class TodoItem
+    public class TodoItemDTO
     {
         public long Id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
-        public string Secret { get; set; }
     }
 }
