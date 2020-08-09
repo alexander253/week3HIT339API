ae8e295f0db0a57ca17d435737e7b9abc6acba9f
unknown <alex_s253@hotmail.com>
2020-08-09 23:32:18 +0930

    completed web api with javascript tutorial

--- WebApplication/Controllers/TodoItemsController.cs
+++ WebApplication/Controllers/TodoItemsController.cs
@@ -116,4 +116,4 @@ namespace WebApplication.Controllers
             };
     }
 }
-}
+
--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -40,6 +40,9 @@ namespace WebApplication
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
